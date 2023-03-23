0c2c953dc25d5b36634786f990ff6c5f6e3bb57f
ron simon <ronsimon100@gmail.com>
2023-03-23 16:00:14 +0200

    added stuff

---
+++ frontend/src/cmps/StayPreview.vue
@@ -0,0 +1,49 @@
+<template>
+  <section>
+    <ul class="home-list">
+       <img
+          class="home-img"
+          :src="`https://a0.muscache.com/im/pictures/e83e702f-ef49-40fb-8fa0-6512d7e26e9b.jpg?aki_policy=large`"
+          alt="img rated stay"
+        />
+       
+        <div class="txt-list">
+          <h3 class="country-name popular">
+      <p>{{ stay.loc.country }},{{ stay.loc.city }}</p> 
+      <p>{{ stay.labels[0]}}</p>
+      <p><strong>${{ stay.price}}</strong> night</p>
+      <p>mar 1-8</p>
+
+          </h3>
+        </div>
+      <!-- </li> -->
+    </ul>
+
+    
+  </section>
+</template>
+<script>
+export default {
+  name: "stay-preview",
+  props: {
+    stay: {
+      type: Object,
+    },
+  },
+
+  computed: {
+    
+  },created() {
+  },
+
+}
+
+</script>
+
+<style>
+.txt-list{
+  line-height: 1px;
+}
+
+</style>
+
--- frontend/src/store/stay-store.js
+++ frontend/src/store/stay-store.js
@@ -1,34 +1,70 @@
 import { stayService } from '../services/stay.service.local.js'
 
 export default {
-    state: {
-        stays: [],
-        filterBy: {
-            country: '',
-            guests: {
-                adults: 0,
-                kids: 0,
-                Infants: 0,
-            },
-            stayTime: '',
-            type: [],
-            price: {
-                minPrice: 30,
-                maxPrice: 150
-            },
-            amenities: [],
-        },
+    state : {
+     stays : [
+        {
+          "_id": "10006546",
+          "name": "Ribeira Charming Duplex",
+          "type": "House",
+          "imgUrls": ["https://a0.muscache.com/im/pictures/e83e702f-ef49-40fb-8fa0-6512d7e26e9b.jpg?aki_policy=large", "otherImg.jpg"],
+          "price": 80.00,
+          "summary": "Fantastic duplex apartment with three bedrooms, located in the historic area of Porto, Ribeira (Cube)...",
+          "capacity": 8,
+          "amenities": [
+            "TV",
+            "Wifi",
+            "Kitchen",
+            "Smoking allowed",
+            "Pets allowed",
+            "Cooking basics"
+          ],
+          "labels": [
+            "Top of the world",
+            "Trending",
+            "Play",
+            "Tropical"
+          ],
+          "host": {
+            "_id": "u101",
+            "fullname": "Davit Pok",
+            "imgUrl": "https://a0.muscache.com/im/pictures/fab79f25-2e10-4f0f-9711-663cb69dc7d8.jpg?aki_policy=profile_small",
+          },
+          "loc": {
+            "country": "Portugal",
+            "countryCode": "PT",
+            "city": "Porto",
+            "address": "17 Kombo st",
+            "lat": -8.61308,
+            "lng": 41.1413
+          },
+          "reviews": [
+            {
+              "id": "madeId",
+              "txt": "Very helpful hosts. Cooked traditional...",
+              "rate": 4,
+              "by": {
+                "_id": "u102",
+                "fullname": "user2",
+                "imgUrl": "/img/img2.jpg"
+              }
+            }
+          ],
+          "likedByUsers": ['mini-user'] // for user-wishlist : use $in
+        }
+      ],
+      filterBy:{}
     },
-    getters: {
-        stays(state) {
-            return state.stays;
+      getters: {
+          stays(state) {
+              return state.stays;
 
         },
     },
         mutations: {
-            setStay(state, { stays }) {
-                state.stays = stays
-            },
+            // setStay(stays) {
+            //     state.stays = stays
+            // },
         },
 
         actions: {
@@ -43,5 +79,7 @@ export default {
             },
 
         }
-    }
+    
 
+
+            }
