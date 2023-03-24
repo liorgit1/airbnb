73c043d07551fdd0f40824ebc88ba696c3f3f428
yiftach <yfty777@gmail.com>
2023-03-24 18:03:18 +0300

    commit

--- frontend/src/cmps/FilterList.vue
+++ frontend/src/cmps/FilterList.vue
@@ -1,5 +1,5 @@
 <template>
-    <section v-if = "!isOpen" class="Filter-list">
+    <section v-if = "!isOpen" class="Filter-list slick-slider ">
     <button class="list-btn">
         <img src="../assets/pngs/icon2.jpg">
     </button>
@@ -39,7 +39,61 @@
 <script>
 
 export default{
-    props: ['isOpen']
+    props: ['isOpen'],
     
+    methods:{
+//         $(document).ready(function() {
+
+//     $('.responsive').slick({
+//     // dots: true,
+//     infinite: true,
+//     speed: 300,
+//     slidesToShow: 8,
+//     slidesToScroll: 1,
+//     responsive: [{
+//         breakpoint: 1024,
+//         settings: {
+//             slidesToShow: 6,
+//             slidesToScroll: 1,
+//             // centerMode: true,
+
+//         }
+
+//     }, {
+//         breakpoint: 800,
+//         settings: {
+//             slidesToShow: 3,
+//             slidesToScroll: 2,
+//             dots: true,
+//             infinite: true,
+
+//         }
+
+
+//     }, {
+//         breakpoint: 600,
+//         settings: {
+//             slidesToShow: 2,
+//             slidesToScroll: 2,
+//             dots: true,
+//             infinite: true,
+            
+//         }
+//     }, {
+//         breakpoint: 480,
+//         settings: {
+//             slidesToShow: 1,
+//             slidesToScroll: 1,
+//             dots: true,
+//             infinite: true,
+//             autoplay: true,
+//             autoplaySpeed: 2000,
+//         }
+//     }]
+// });
+
+
+// })
+    }
 }
 </script>
