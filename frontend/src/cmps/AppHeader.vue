3660e4acf7339f8776d491a0891b1bd61ae13a11
yiftach <yfty777@gmail.com>
2023-03-25 15:33:28 +0300

    yiftach-commit

--- frontend/src/cmps/AppHeader.vue
+++ frontend/src/cmps/AppHeader.vue
@@ -1,8 +1,8 @@
 <template>
-  <header>
+  <header >
     <nav class="flex">
-      <RouterLink @click = "isOpen = false" to="/" class="logo">
-      <!-- <RouterLink to="/" class="logo"> -->
+      <RouterLink to="/" @click = "isOpen = false && resetParams()"  class="logo">
+        <RouterLink to="/" class="logo" />  
         <img class="logo-img" src="../assets/pngs/logo1.png">
         <span class="logo-text">Earthbnb</span>
       </RouterLink>
@@ -12,7 +12,7 @@
         <span>|</span>
         <button>anyweek</button>
         <span>|</span>
-        <button>add guests</button>
+        <button style="font-weight: normal">add guests</button>
         <button class="search-btn"> <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style="display: block; fill: none; height: 12px; width: 12px; stroke: currentcolor; stroke-width: 5.33333; overflow: visible;"><g fill="none"><path d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"></path></g></svg></button>
       </div>
 
@@ -37,9 +37,9 @@
       <span>{{ loggedInUser.score.toLocaleString() }}</span>
       <img :src="loggedInUser.imgUrl" />
     </section>
-    
+    <hr class="solid" v-if="!isOpen">
   </header>
-  <hr class="solid" v-if="!isOpen">
+   <!-- <hr class="boundry" v-if="!isOpen">  -->
   <!-- <modal v-if="isOpen" class="back-drop">
     <div style="background-color: white;">.</div>
   </modal> -->
@@ -62,6 +62,11 @@ export default {
   },
 
   computed: {
+
+    resetParams(){
+      this.$router.replace({'query': null});
+
+    },
     loggedInUser() {
       return this.$store.getters.loggedinUser
     },
