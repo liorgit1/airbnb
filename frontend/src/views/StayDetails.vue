5cb097bfd08c44a976437240fb7ff67ea657a734
liorgit1 <liorlvovsky@gmail.com>
2023-03-26 19:35:31 +0300

    fixing details layoutAND STUF

--- frontend/src/views/StayDetails.vue
+++ frontend/src/views/StayDetails.vue
@@ -1,33 +1,45 @@
 <template>
-    <section v-if="stay" class="stay-details details-layout" @click="this.showModal=false">
+    <section v-if="stay" class="stay-details details-layout" @click="this.showModal = false">
 
 
         <!-- <section> -->
-            <divg class="gc23">
+        <divg class="gc23" style="height: 76px;">
 
-                <h1> {{ stay.name }}</h1>
-                <div class="likeAndShare">
-                    <p class="placeLink">{{ stay.loc.city }}, {{ stay.loc.country }}</p>
-
-                    <a>like</a>
+            <h1> {{ stay.name }}</h1>
+            <div class="likeAndShare">
+                <p class="placeLink fs16">  <img src="../assets/pngs/star.png    " alt="">  {{ stay.loc.city }}, {{ stay.loc.country }}</p>
+                <span class="fs16">
+                    <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation"
+                        focusable="false"
+                        style="fill: none; height: 16px; width: 16px; stroke: currentcolor; stroke-width: 2; overflow: visible;">
+                        <g fill="none">
+                            <path d="M27 18v9a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-9"></path>
+                            <path d="M16 3v23V3z"></path>
+                            <path d="M6 13l9.293-9.293a1 1 0 0 1 1.414 0L26 13"></path>
+                        </g>
+                    </svg>
                     <a>share</a>
-                </div>
-            </divg>
-            <div class="detailsImgContainer">
-                <img :src="stay.imgUrls[0]" class="img img1">
-                <img :src="stay.imgUrls[1] || stay.imgUrls[0]" class="img img2">
-                <img :src="stay.imgUrls[2] || stay.imgUrls[0]" class="img img3">
-                <img :src="stay.imgUrls[3] || stay.imgUrls[0]" class="img img4">
-                <img :src="stay.imgUrls[4] || stay.imgUrls[0]" class="img mainIgm">
+                </span>
+                <span class="fs16">
+                    <a> <img style="width:16px ; " src="../assets/pngs/heart.png"> Save</a>
+                </span>
             </div>
-            <section class="info-container">
-                <GuestsModal  v-if="showModal" />
-                <Reservation @click.stop :stay="stay" @openModal="this.showModal = true" />
-                <div class="flex-col">
-                    <StayInfo :stay="stay" />
-                    <StayAmenities :stay="stay" />
-                </div>
-            </section>
+        </divg>
+        <div class="detailsImgContainer">
+            <img :src="stay.imgUrls[0]" class="img img1">
+            <img :src="stay.imgUrls[1] || stay.imgUrls[0]" class="img img2">
+            <img :src="stay.imgUrls[2] || stay.imgUrls[0]" class="img img3">
+            <img :src="stay.imgUrls[3] || stay.imgUrls[0]" class="img img4">
+            <img :src="stay.imgUrls[4] || stay.imgUrls[0]" class="img mainIgm">
+        </div>
+        <section class="info-container">
+            <GuestsModal v-if="showModal" />
+            <Reservation @click.stop :stay="stay" @openModal="this.showModal = true" />
+            <div class="flex-col">
+                <StayInfo :stay="stay" />
+                <StayAmenities :stay="stay" />
+            </div>
+        </section>
 
 
         <!-- </section> -->
@@ -49,7 +61,7 @@ import { stayService } from '../services/stay.service.local.js'
 
 
 export default {
-   
+
     data() {
         return {
             showModal: false,
@@ -81,7 +93,7 @@ export default {
     }
     , computed: {
 
-    
+
         // stay() {
         //     return this.$store.getters.stays[0]
         // }
