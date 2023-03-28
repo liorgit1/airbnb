5eabc6775d743195c8fb619587e170713704f2d4
yiftach <yfty777@gmail.com>
2023-03-28 15:00:28 +0300

    yiftach-commit

---
+++ frontend/src/cmps/DatePickerModal.vue
@@ -0,0 +1,21 @@
+<template>
+<container>
+<Datepicker />
+
+</container>
+</template>
+
+<script>
+
+import 'vue-datepicker-ui/lib/vuedatepickerui.css';
+import VueDatepickerUi from 'vue-datepicker-ui';
+
+export default{
+components:{
+
+Datepicker: VueDatepickerUi    
+
+}
+
+}
+</script>
