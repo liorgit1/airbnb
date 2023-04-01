89230bf1cd5f5851a86fd34d40ccdcfab5668ecb
yiftach <yfty777@gmail.com>
2023-04-01 22:03:02 +0300

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,17 +3,23 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),
-  AutoImport({
-    resolvers: [ElementPlusResolver()],
-  }),
-  Components({
-    resolvers: [ElementPlusResolver()],
-  }),'import-analysis: { disabled: false   }'],
-  build:{
-    outDir:'../backend',
-    emptyOutDir:true
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+    {
+      importAnalysis: { disabled: true }
+    }
+  ],
+  build: {
+    outDir: '../backend',
+    emptyOutDir: true
   }
 })
