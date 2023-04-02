a939b6dae53b528ae25f445942372d5838e6ebbe
ron simon <ronsimon100@gmail.com>
2023-04-02 19:13:40 +0300

    123

--- backend/config/prod.js
+++ backend/config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
   
-  dbURL:  process.env.DB_KEY,
+  dbURL:  process.env.DB_KEY || 'mongodb+srv://yfty777:YiftachZ15i@mycloudservice.mafxmux.mongodb.net/?retryWrites=true&w=majority',
   dbName: 'stay_db'
   
 }
