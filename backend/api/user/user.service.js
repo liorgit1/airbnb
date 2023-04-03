a5339c9f8f81404656ae2af63a65e2a33691f1b7
ron simon <ronsimon100@gmail.com>
2023-04-03 17:11:18 +0300

    iii

--- backend/api/user/user.service.js
+++ backend/api/user/user.service.js
@@ -71,7 +71,7 @@ async function update(user) {
             _id: ObjectId(user._id),
             username: user.username,
             fullname: user.fullname,
-            score: user.score
+            likedStays: [],
         }
         const collection = await dbService.getCollection('user')
         await collection.updateOne({ _id: userToSave._id }, { $set: userToSave })
@@ -93,8 +93,9 @@ async function add(user) {
             username: user.username,
             password: user.password,
             fullname: user.fullname,
-            wishlist: [],
-            reservations: [],
+            likedStays: [],
+            
+            
             
         }
         const collection = await dbService.getCollection('user')
